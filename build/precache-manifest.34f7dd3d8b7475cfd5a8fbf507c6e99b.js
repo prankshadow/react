1b25fde5d4da0035c8db4a716b74f3929ee8e043
prankshadow <106470767+prankshadow@users.noreply.github.com>
2022-11-14 18:50:32 +0530

    Full blank master

---
+++ build/precache-manifest.34f7dd3d8b7475cfd5a8fbf507c6e99b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "686ca38ac30c191df147",
+    "url": "/static/js/main.686ca38a.chunk.js"
+  },
+  {
+    "revision": "6228474a634f18b25b4b",
+    "url": "/static/js/1.6228474a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b41ed60cf7b13a6f513adcff91afc8e",
+    "url": "/index.html"
+  }
+];
---
+++ build/static/js/main.686ca38a.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,n,o){e.exports=o(4)},function(e,n,o){"use strict";o.r(n);var t=o(0),r=o.n(t),c=o(2);o.n(c).a.render(r.a.createElement("h1",null,"Hello World"),document.getElementById("root"))}],[[3,2,1]]]);
+//# sourceMappingURL=main.686ca38a.chunk.js.map
