e6576b5f0de6af49624e7ec83ce3c35371852d1b
prankshadow <106470767+prankshadow@users.noreply.github.com>
2022-12-06 14:59:39 +0530

    6-12-2022 Mapping components practice

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-// <-----------OLD------------>
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./components/App";
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-// <-----------NEW----------->
-
 import React from "react";
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
 import App from "./components/App";
 
-createRoot(document.getElementById('root')).render(<App />)
-
-
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
