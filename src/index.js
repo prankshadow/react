b4c535bebcb5e38b19fd7f2def493e035c13c943
prankshadow <106470767+prankshadow@users.noreply.github.com>
2022-12-06 14:54:29 +0530

    6-12-2022

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+// <-----------OLD------------>
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// <-----------NEW----------->
+
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client'
 import App from "./components/App";
 
+createRoot(document.getElementById('root')).render(<App />)
 
-ReactDOM.render (
-    <App />,
-    document.getElementById('root')
-);
+
 
 
